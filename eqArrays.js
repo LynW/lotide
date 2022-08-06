c4798df0001b1b864eb4a021e821eae0d19d2482
lynw <c54wang@gmail.com>
2022-08-06 19:00:38 +0000

    implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+
+  let result = false;
+
+  if (actual === expected) {
+    result = true;
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    result = false;
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+  return result;
+
+};
+
+const eqArrays = function(array1, array2) {
+
+  let result = false;
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      result = true;
+    } else {
+      result = false;
+    }
+  }
+  
+  return result;
+
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
