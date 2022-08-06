19ec97c8a8c09abd2795fa7e70801d230b2c7d5b
lynw <c54wang@gmail.com>
2022-08-06 18:26:06 +0000

    implement head

--- head.js
+++ head.js
@@ -15,5 +15,11 @@ const assertEqual = function(actual, expected) {
 };
 
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const head = function(arrayList) {
+
+  return arrayList[0];
+
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
