5608f07aa1da52ee46b4c068ae5b2d470e39c4b6
lynw <c54wang@gmail.com>
2022-08-07 18:18:56 +0000

    refactor tail

--- tail.js
+++ tail.js
@@ -1,6 +1,3 @@
-const assertEqual = require('./assertEqual');
-
-
 const tail = function(arrayList) {
 
   let finalArray = [];
@@ -17,7 +14,5 @@ const tail = function(arrayList) {
 
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
+
