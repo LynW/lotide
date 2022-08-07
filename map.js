42ea663f6a54999b6bea91b8f2a39dc9c828b78e
lynw <c54wang@gmail.com>
2022-08-07 17:30:05 +0000

    implement map

---
+++ map.js
@@ -0,0 +1,52 @@
+const eqArrays = function(array1, array2) {
+
+  let result = false;
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      result = true;
+    } else {
+      result = false;
+    }
+  }
+  
+  return result;
+
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  let result = false;
+
+  if (eqArrays(actual, expected)) {
+    result = true;
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    result = false;
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+  return result;
+
+};
+
+const map = function(array, callback) {
+  const results = []
+
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+
+assertArraysEqual(results1,[ 'g', 'c', 't', 'm', 't' ] );
